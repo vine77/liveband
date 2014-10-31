f12693232725249e9b65c8dc4942f1d97b37b435
ramtin <ramtinb@gmail.com>
2014-10-31 14:30:34 -0400

    disable/enable guitar amp

--- app/views/guitar-amplifier.js
+++ app/views/guitar-amplifier.js
@@ -11,5 +11,21 @@ export default Ember.View.extend({
       .attr('data-width', '150')
       .attr('data-height', '150')
       .knob();
+
+	this.$("#vol").click(function(){
+		if ($("#vol").attr("class") == "fa fa-volume-up fa-2x"){
+			$("#vol").removeClass("fa fa-volume-up fa-2x").addClass("fa fa-volume-off fa-2x");
+			$('.dial').val(0).trigger('change');
+			$('#status').text('Enable');
+		}
+		else{
+			$("#vol").removeClass("fa fa-volume-off fa-2x").addClass("fa fa-volume-up fa-2x");
+			$(".dial").attr('data-max', '10');
+			$('.dial').val(7).trigger('change');		
+			$('#status').text('Disable');			
+		}
+	});
+
   }
+
 });
