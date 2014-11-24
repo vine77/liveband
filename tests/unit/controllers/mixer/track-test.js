f307f2ac38634386504dca78d8c175bb76035dce
Nathan Ward <ward@digivine.com>
2014-11-23 19:32:38 -0800

    Update mixer tests

--- tests/unit/controllers/mixer/track-test.js
+++ tests/unit/controllers/mixer/track-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleFor('controller:mixer/track', 'MixerTrackController', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['controller:mixer']
 });
 
 // Replace this with your real tests.
